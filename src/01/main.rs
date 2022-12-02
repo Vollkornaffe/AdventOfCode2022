1c259bfbf192d8347a3633cdd11c4fae14e934fb
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-02 23:30:21 +0100

    Solve 01, part one

--- src/01/main.rs
+++ src/01/main.rs
@@ -1,3 +1,24 @@
+use std::path::Path;
+
+use common::read_lines;
+
+fn solve<P: AsRef<Path>>(filename: P) {
+    let sums = read_lines(filename)
+        .unwrap()
+        .iter()
+        .fold(vec![0], |mut elve_sums, next_item| {
+            if next_item.is_empty() {
+                elve_sums.push(0);
+                elve_sums
+            } else {
+                *elve_sums.last_mut().unwrap() += next_item.parse::<usize>().unwrap();
+                elve_sums
+            }
+        });
+    println!("{}", sums.iter().max().unwrap());
+}
+
 fn main() {
-    println!("Hello, world!");
+    solve("src/01/example");
+    solve("src/01/input");
 }
