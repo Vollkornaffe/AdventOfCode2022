2236f285999c1f4124334c6a78e5d870b004f335
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-03 00:21:19 +0100

    Solve part two

--- src/02/main.rs
+++ src/02/main.rs
@@ -1,15 +1,18 @@
 use common::read_lines;
 use std::path::Path;
 
-const LOOSE: usize = 0;
-const DRAW: usize = 3;
-const WIN: usize = 6;
-
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 enum Move {
     Rock,
     Paper,
     Scissors,
+}
+
+#[derive(Copy, Clone)]
+enum Goal {
+    Loose,
+    Draw,
+    Win,
 }
 
 fn map_move(s: &str) -> Move {
@@ -24,7 +27,40 @@ fn map_move(s: &str) -> Move {
     }
 }
 
+fn map_goal(s: &str) -> Goal {
+    match s {
+        "X" => Goal::Loose,
+        "Y" => Goal::Draw,
+        "Z" => Goal::Win,
+        _ => unreachable!(),
+    }
+}
+
+fn map_move_goal(m: Move, g: Goal) -> Move {
+    match m {
+        Move::Rock => match g {
+            Goal::Loose => Move::Scissors,
+            Goal::Draw => Move::Rock,
+            Goal::Win => Move::Paper,
+        },
+        Move::Paper => match g {
+            Goal::Loose => Move::Rock,
+            Goal::Draw => Move::Paper,
+            Goal::Win => Move::Scissors,
+        },
+        Move::Scissors => match g {
+            Goal::Loose => Move::Paper,
+            Goal::Draw => Move::Scissors,
+            Goal::Win => Move::Rock,
+        },
+    }
+}
+
 fn score_match(other: Move, me: Move) -> usize {
+    const LOOSE: usize = 0;
+    const DRAW: usize = 3;
+    const WIN: usize = 6;
+
     0 + match me {
         Move::Rock => 1,
         Move::Paper => 2,
@@ -42,7 +78,7 @@ fn score_match(other: Move, me: Move) -> usize {
     }
 }
 
-fn calc_score<P: AsRef<Path>>(filename: P) -> usize {
+fn calc_score_part_one<P: AsRef<Path>>(filename: P) -> usize {
     read_lines(filename).unwrap().iter().fold(0, |score, line| {
         let mut split = line.split(" ");
         score
@@ -52,7 +88,23 @@ fn calc_score<P: AsRef<Path>>(filename: P) -> usize {
             )
     })
 }
+
+fn calc_score_part_two<P: AsRef<Path>>(filename: P) -> usize {
+    read_lines(filename).unwrap().iter().fold(0, |score, line| {
+        let mut split = line.split(" ");
+        let other = map_move(split.next().unwrap());
+        let goal = map_goal(split.next().unwrap());
+        let me = map_move_goal(other, goal);
+        score + score_match(other, me)
+    })
+}
+
 fn main() {
-    println!("{}", calc_score("src/02/example"));
-    println!("{}", calc_score("src/02/input"));
+    println!("Part one:");
+    println!("{}", calc_score_part_one("src/02/example"));
+    println!("{}", calc_score_part_one("src/02/input"));
+
+    println!("Part two:");
+    println!("{}", calc_score_part_two("src/02/example"));
+    println!("{}", calc_score_part_two("src/02/input"));
 }
