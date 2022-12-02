1a883e6de8b55622691cd07a2cc22f1a8b6e82ad
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-02 23:36:32 +0100

    Solve part two

--- src/01/main.rs
+++ src/01/main.rs
@@ -2,8 +2,8 @@ use std::path::Path;
 
 use common::read_lines;
 
-fn solve<P: AsRef<Path>>(filename: P) {
-    let sums = read_lines(filename)
+fn get_sums<P: AsRef<Path>>(filename: P) -> Vec<usize> {
+    read_lines(filename)
         .unwrap()
         .iter()
         .fold(vec![0], |mut elve_sums, next_item| {
@@ -14,11 +14,27 @@ fn solve<P: AsRef<Path>>(filename: P) {
                 *elve_sums.last_mut().unwrap() += next_item.parse::<usize>().unwrap();
                 elve_sums
             }
-        });
-    println!("{}", sums.iter().max().unwrap());
+        })
+}
+
+fn solve_part_one<P: AsRef<Path>>(filename: P) {
+    println!("{}", get_sums(filename).iter().max().unwrap());
+}
+
+fn solve_part_two<P: AsRef<Path>>(filename: P) {
+    let mut sums = get_sums(filename);
+    sums.sort();
+    sums.reverse();
+
+    println!("{}", sums.iter().take(3).sum::<usize>());
 }
 
 fn main() {
-    solve("src/01/example");
-    solve("src/01/input");
+    println!("Part one:");
+    solve_part_one("src/01/example");
+    solve_part_one("src/01/input");
+
+    println!("Part two:");
+    solve_part_two("src/01/example");
+    solve_part_two("src/01/input");
 }
