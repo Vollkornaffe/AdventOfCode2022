c4f043e1ef65369959454bdaf88423e710c50be5
Vollkornaffe <larshelge.scheel@gmail.com>
2022-12-03 13:23:53 +0100

    Solve 03, part one

--- src/03/main.rs
+++ src/03/main.rs
@@ -1,3 +1,53 @@
+use common::read_lines;
+use std::{collections::HashSet, path::Path};
+
+fn alphabet() -> Vec<char> {
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .collect()
+}
+
+#[derive(Copy, Clone, Hash, PartialEq, Eq)]
+struct Item(char);
+impl Item {
+    fn prio(&self) -> usize {
+        1 + alphabet().iter().position(|&c| c == self.0).unwrap()
+    }
+}
+
+struct RuckSack(HashSet<Item>, HashSet<Item>);
+
+impl RuckSack {
+    fn new(s: &String) -> Self {
+        let chars: Vec<char> = s.chars().collect();
+        let mut halves = chars.as_slice().chunks(chars.len() / 2);
+        Self(
+            halves.next().unwrap().iter().map(|&c| Item(c)).collect(),
+            halves.next().unwrap().iter().map(|&c| Item(c)).collect(),
+        )
+    }
+
+    fn duplicate(&self) -> Item {
+        *self.0.intersection(&self.1).next().unwrap()
+    }
+}
+
+fn read_rucksacks<P: AsRef<Path>>(filename: P) -> Vec<RuckSack> {
+    read_lines(filename)
+        .unwrap()
+        .iter()
+        .map(RuckSack::new)
+        .collect()
+}
+
+fn solve_part_one<P: AsRef<Path>>(filename: P) -> usize {
+    read_rucksacks(filename)
+        .iter()
+        .map(|rs| rs.duplicate().prio())
+        .sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", solve_part_one("src/03/example"));
+    println!("{}", solve_part_one("src/03/input"));
 }
