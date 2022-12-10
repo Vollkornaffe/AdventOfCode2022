70ff268a22c009862ae92442c6acf62155aa42f7
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-10 17:50:50 +0100

    Solve part two

--- src/10/main.rs
+++ src/10/main.rs
@@ -1,7 +1,7 @@
 use common::read_lines;
 
-fn solve_part_one(lines: &[String]) {
-    let register_values = lines
+fn get_register_values(lines: &[String]) -> Vec<i32> {
+    lines
         .iter()
         .fold((Vec::new(), 1), |(mut states, mut next), line| {
             if line == "noop" {
@@ -22,8 +22,11 @@ fn solve_part_one(lines: &[String]) {
 
             (states, next)
         })
-        .0;
+        .0
+}
 
+fn solve_part_one(lines: &[String]) {
+    let register_values = get_register_values(lines);
     println!(
         "{}",
         20 * register_values[19]
@@ -35,7 +38,32 @@ fn solve_part_one(lines: &[String]) {
     );
 }
 
+fn solve_part_two(lines: &[String]) {
+    let register_values = get_register_values(lines);
+    let mut cycle = 0;
+    for _ in 0..6 {
+        for i in 0..40 {
+            print!(
+                "{}",
+                if (register_values[cycle] - i).abs() < 2 {
+                    "#"
+                } else {
+                    "."
+                }
+            );
+
+            cycle += 1;
+        }
+        println!();
+    }
+}
+
 fn main() {
+    println!("Part one:");
     solve_part_one(&read_lines("src/10/example").unwrap());
     solve_part_one(&read_lines("src/10/input").unwrap());
+
+    println!("Part one:");
+    solve_part_two(&read_lines("src/10/example").unwrap());
+    solve_part_two(&read_lines("src/10/input").unwrap());
 }
