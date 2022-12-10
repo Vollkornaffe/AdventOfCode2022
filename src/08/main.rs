49786c65bf760328bc1beba1f2d2760390463ef6
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-10 01:50:59 +0100

    Solve part two

--- src/08/main.rs
+++ src/08/main.rs
@@ -55,7 +55,72 @@ fn solve_part_one(lines: &[String]) {
     println!("{}", visible.len());
 }
 
+fn parse_to_grid(lines: &[String]) -> Vec<Vec<u32>> {
+    lines
+        .iter()
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap() + 1).collect())
+        .collect()
+}
+
+fn solve_part_two(lines: &[String]) {
+    let grid = parse_to_grid(lines);
+
+    let mut score = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            let h = grid[y][x];
+
+            score = score.max(
+                {
+                    let mut nx = x;
+                    while nx != 0 {
+                        nx -= 1;
+                        if !(grid[y][nx] < h) {
+                            break;
+                        }
+                    }
+                    x - nx
+                } * {
+                    let mut nx = x;
+                    while nx != grid.len() - 1 {
+                        nx += 1;
+                        if !(grid[y][nx] < h) {
+                            break;
+                        }
+                    }
+                    nx - x
+                } * {
+                    let mut ny = y;
+                    while ny != 0 {
+                        ny -= 1;
+                        if !(grid[ny][x] < h) {
+                            break;
+                        }
+                    }
+                    y - ny
+                } * {
+                    let mut ny = y;
+                    while ny != grid.len() - 1 {
+                        ny += 1;
+                        if !(grid[ny][x] < h) {
+                            break;
+                        }
+                    }
+                    ny - y
+                },
+            );
+        }
+    }
+
+    println!("{}", score);
+}
+
 fn main() {
+    println!("Part one:");
     solve_part_one(&read_lines("src/08/example").unwrap());
     solve_part_one(&read_lines("src/08/input").unwrap());
+
+    println!("Part two:");
+    solve_part_two(&read_lines("src/08/example").unwrap());
+    solve_part_two(&read_lines("src/08/input").unwrap());
 }
