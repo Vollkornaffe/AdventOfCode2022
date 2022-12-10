1b161c1062991a0793200cc180468ffc523dc93f
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-10 17:22:35 +0100

    Solve part two
    The description wasn't really clear IMHO..
    You have to do the follow movements one step at a time, but it could
    just as well mean that you first let each knot do the 'full follow'
    movement and then go further down the line.
    
    Oh well, I guess I should have studied the example more

--- src/09/main.rs
+++ src/09/main.rs
@@ -2,11 +2,21 @@ use std::collections::HashSet;
 
 use common::read_lines;
 
-fn solve_part_one(lines: &[String]) {
+fn solve(num_knots: usize, lines: &[String]) {
     let mut visited = HashSet::new();
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
-    visited.insert(tail);
+    let mut knots = vec![(0, 0); num_knots];
+
+    visited.insert(knots.last().cloned().unwrap());
+
+    let follow = |head: &(i32, i32), tail: &mut (i32, i32)| -> bool {
+        if (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
+            tail.0 += (head.0 - tail.0).clamp(-1, 1);
+            tail.1 += (head.1 - tail.1).clamp(-1, 1);
+            true
+        } else {
+            false
+        }
+    };
 
     for make_move in lines.iter().map(|line| {
         let mut split = line.split(" ");
@@ -20,17 +30,43 @@ fn solve_part_one(lines: &[String]) {
             _ => unreachable!(),
         }
     }) {
-        make_move(&mut head);
-        while (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
-            tail.0 += (head.0 - tail.0).clamp(-1, 1);
-            tail.1 += (head.1 - tail.1).clamp(-1, 1);
-            visited.insert(tail);
+        make_move(&mut knots[0]);
+        loop {
+            let mut changed = false;
+            let mut head = knots[0];
+            for i in 1..knots.len() {
+                if follow(&head, &mut knots[i]) {
+                    changed = true;
+                }
+                head = knots[i];
+            }
+            visited.insert(knots.last().cloned().unwrap());
+            if !changed {
+                break;
+            }
         }
     }
+
+    for y in -50..50 {
+        for x in -50..50 {
+            if visited.contains(&(x, -y)) {
+                print!("#")
+            } else {
+                print!(".")
+            }
+        }
+        println!();
+    }
+
     println!("{}", visited.len());
 }
 
 fn main() {
-    solve_part_one(&read_lines("src/09/example").unwrap());
-    solve_part_one(&read_lines("src/09/input").unwrap());
+    println!("Part one:");
+    solve(2, &read_lines("src/09/example").unwrap());
+    solve(2, &read_lines("src/09/input").unwrap());
+
+    println!("Part two:");
+    solve(10, &read_lines("src/09/example2").unwrap());
+    solve(10, &read_lines("src/09/input").unwrap());
 }
