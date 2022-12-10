1fefc6ddf67bead8f7b05df5257cb0d8b0c65d2e
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-10 17:43:54 +0100

    Solve part one

--- src/10/main.rs
+++ src/10/main.rs
@@ -1,3 +1,41 @@
+use common::read_lines;
+
+fn solve_part_one(lines: &[String]) {
+    let register_values = lines
+        .iter()
+        .fold((Vec::new(), 1), |(mut states, mut next), line| {
+            if line == "noop" {
+                states.push(next);
+                return (states, next);
+            }
+
+            states.push(next);
+            states.push(next);
+
+            next += line
+                .split(" ")
+                .skip(1)
+                .next()
+                .unwrap()
+                .parse::<i32>()
+                .unwrap();
+
+            (states, next)
+        })
+        .0;
+
+    println!(
+        "{}",
+        20 * register_values[19]
+            + 60 * register_values[59]
+            + 100 * register_values[99]
+            + 140 * register_values[139]
+            + 180 * register_values[179]
+            + 220 * register_values[219]
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    solve_part_one(&read_lines("src/10/example").unwrap());
+    solve_part_one(&read_lines("src/10/input").unwrap());
 }
