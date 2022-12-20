cd2706ae766dcd29c0db0fe0bf1d8ce52c5a7a48
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-20 18:24:15 +0100

    Solve part two

--- src/20/main.rs
+++ src/20/main.rs
@@ -1,16 +1,10 @@
 use common::read_lines;
 
-fn solve_part_one(lines: &[String]) {
-    let mut mix = lines
-        .iter()
-        .enumerate()
-        .map(|(idx, s)| (idx, s.parse::<isize>().unwrap()))
-        .collect::<Vec<_>>();
+fn mix_mix(mix: &mut [(usize, isize)]) {
     let n = mix.len();
-
     for idx in 0..n {
         let mut i = mix.iter().position(|e| e.0 == idx).unwrap();
-        let mut dir = mix[i].1;
+        let mut dir = mix[i].1 % (n as isize - 1);
         while dir != 0 {
             let next = if dir < 0 {
                 if i == 0 {
@@ -30,7 +24,10 @@ fn solve_part_one(lines: &[String]) {
             dir -= dir.clamp(-1, 1);
         }
     }
+}
 
+fn print_solution(mix: &[(usize, isize)]) {
+    let n = mix.len();
     let zero = mix.iter().position(|e| e.1 == 0).unwrap();
     println!(
         "{}",
@@ -38,7 +35,33 @@ fn solve_part_one(lines: &[String]) {
     );
 }
 
+fn solve_part_one(lines: &[String]) {
+    let mut mix = lines
+        .iter()
+        .enumerate()
+        .map(|(idx, s)| (idx, s.parse::<isize>().unwrap()))
+        .collect::<Vec<_>>();
+    mix_mix(&mut mix);
+    print_solution(&mix);
+}
+
+fn solve_part_two(lines: &[String]) {
+    let mut mix = lines
+        .iter()
+        .enumerate()
+        .map(|(idx, s)| (idx, s.parse::<isize>().unwrap() * 811589153))
+        .collect::<Vec<_>>();
+    for _ in 0..10 {
+        mix_mix(&mut mix);
+    }
+    print_solution(&mix);
+}
 fn main() {
+    println!("Part one:");
     solve_part_one(&read_lines("src/20/example").unwrap());
     solve_part_one(&read_lines("src/20/input").unwrap());
+
+    println!("Part two:");
+    solve_part_two(&read_lines("src/20/example").unwrap());
+    solve_part_two(&read_lines("src/20/input").unwrap())
 }
